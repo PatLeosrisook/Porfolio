4b274b21d5999e788afcc642b8acd54d396a2082
Pat leosrisook <49037327+PatLeosrisook@users.noreply.github.com>
2023-08-25 22:22:03 +0700

    manage to delete range out of the array

--- Frontends/React/password-generator-app/password-generator/src/Components/Generator.js
+++ Frontends/React/password-generator-app/password-generator/src/Components/Generator.js
@@ -1,104 +1,151 @@
  import { faCopy } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Slider, { SliderThumb, SliderValueLabelProps } from '@mui/material/Slider';
 export default function Generator() {
     const [condition, setCondition] = useState({
-        includeUpper: false,
-        includeLower: true,
-        includeNumber: false,
-        includeSymbol:false
+        includeUpper:  { 
+            is: false,
+            range: [65,90]
+        },
+        includeLower:  { 
+            is: false,
+            range: [97,122]
+        },
+        includeNumber: { 
+            is: false,
+            range: [48,57]
+        },
+        includeSymbol: { 
+            is: false,
+            range: [[33,47],[58,64],[94,95], [124,126]]
+        }
     })
     const [length, setLength] = useState(0)
     const [randomPassword, setRandomPassword] = useState("dfjadklsfj")
+    const[arrayOfSelection, setArrayOfSelection] = useState([])
+    let removeItem = (itemToRemove, items) => {
+        console.log("reemoving", arrayOfSelection)
+        let indexOfRemoval = arrayOfSelection.filter((item, index) => {
+            console.log("removing items from", arrayOfSelection, 'and item to remove is ', itemToRemove)
+            if(JSON.stringify(item) == JSON.stringify(itemToRemove)){
+                console.log(item, itemToRemove)
+                return index
+            }
+        })
+        arrayOfSelection.splice(indexOfRemoval, 1)
+        
+        console.log('removed?', indexOfRemoval, itemToRemove, arrayOfSelection)
+    }
     let randomizer = () => {
-        console.log("Randomizing....", length)
+        console.log("Randomizing....", length, 'currentOb', condition, arrayOfSelection)
         let password = ""
         //ascii code for characters? 
-        let arrayOfSelection = [[97,122]]
-        if(condition.includeLower) {
-            console.log("have lower")
-
-        }  else {
-            
-        }
-        if(condition.includeUpper) {
-            console.log("have upper")
-            arrayOfSelection.push([65,90])
-            
-        } else {
-            console.log("don't have upper")
-        } 
-        if(condition.includeNumber) {
-            console.log("have number")
-            arrayOfSelection.push([48,57])
-            
-        } else {
-            console.log("don't have number")
-        } 
-        if(condition.includeSymbol) {
-            console.log("have symbol")
-            arrayOfSelection.push([[33,47],[58,64],[94,95], [124,126]])
-
-        } else {
-            console.log("don't have sybol")
-        }
+        // let arrayOfSelection = [[97,122]]
+        
         for(let i = 0 ; i < length; i ++) {
-            let randomSelection = Math.floor(Math.random() * ((arrayOfSelection.length ) - 0) + 0)
-            // let randomSelection = 2
-            console.log(arrayOfSelection[randomSelection], randomSelection,typeof arrayOfSelection[randomSelection][0])
+            let randomSelection = Math.max(Math.floor(Math.random() * arrayOfSelection.length ), 0)
+            
+            // console.log("Selectin:", randomSelection )
             let randomChar = null
             if(typeof arrayOfSelection[randomSelection][0] !== 'number'){
-                //then this have a sub array.
                 
-                randomSelection = Math.floor(Math.random() * ((Math.max(arrayOfSelection[2].length - 1, 0) - 0) + 0)) //selecting random sub array
-                randomChar = Math.floor(Math.random() * (arrayOfSelection[2][randomSelection][1] - arrayOfSelection[2][randomSelection][0])  + arrayOfSelection[2][randomSelection][0]) // selecting random ascii number between 2 nums in the sub
-                // console.log("array[randomSe]:",arrayOfSelection[2][randomSelection], "\n", "random selection:", randomSelection,'\n', "arrayOfselection",  arrayOfSelection, '\n', "string ascii", String.fromCharCode(randomChar),'\n', 'random char:', randomChar)
+                //then this have a sub array.
+                console.log("symbol array encountered")
+                let randomSub = Math.floor(Math.random() * arrayOfSelection[randomSelection].length ) //selecting random sub array
+                randomChar = Math.floor(Math.random() * (arrayOfSelection[randomSelection][randomSub][1] - arrayOfSelection[randomSelection][randomSub][0])  + arrayOfSelection[randomSelection][randomSub][0]) // selecting random ascii number between 2 nums in the sub
+                console.log(arrayOfSelection[randomSelection][randomSub], randomSub, "random char", randomChar, ":", String.fromCharCode(randomChar))
                 password += String.fromCharCode(randomChar)
             } else {
 
                 randomChar = Math.floor(Math.random() * (arrayOfSelection[randomSelection][1] - arrayOfSelection[randomSelection][0]) + arrayOfSelection[randomSelection][0] )
                 password += String.fromCharCode(randomChar)
-                console.log("normal character\n", "random char:", randomChar, "or", String.fromCharCode(randomChar), "\n", "random selection:", randomSelection)
             }
             
         }
         setRandomPassword(password)
     }
     let handleChange = (e) => {
-        let condition = e.target.name
-        let currentObject = {...condition}
-        if(e.target.checked) {
-
-            if(condition == 'uppercase') {
-                console.log("upper")
-                currentObject.incudeUpper = true
-                setCondition(currentObject)
-            } else if(condition == 'lowercase') {
-                console.log("lower")
-                currentObject.incudeLower = true
-                setCondition(currentObject)
-                
-            } else if(condition == 'number') {
-                console.log("number")
-                currentObject.incudeNumber = true
-                setCondition(currentObject)
-                
-            } else if(condition == 'passwordLength') {
-                
+        let conditionName = e.target.name;
+        let currentObject = { ...condition }; // Make sure to create a copy of the state object
+        
+        if (e.target.checked) {
+            if (conditionName === 'uppercase') {
+                currentObject.includeUpper.is = true;
+            } else if (conditionName === 'lowercase') {
+                currentObject.includeLower.is = true;
+            } else if (conditionName === 'number') {
+                currentObject.includeNumber.is = true;
+            } else if(conditionName == 'symbol') {
+                currentObject.includeSymbol.is = true;
             }
-            else {
-                
-                console.log("symbol")
-                currentObject.incudeSymbol = true
-                setCondition(currentObject)
+        } else {
+            // Handle the case when the checkbox is unchecked and update the corresponding property to false
+            if (conditionName === 'uppercase') {
+                currentObject.includeUpper.is = false;
+                removeItem(currentObject.includeUpper.range)
+            } else if (conditionName === 'lowercase') {
+                currentObject.includeLower.is = false;
+                removeItem(currentObject.includeLower.range)
+            } else if (conditionName === 'number') {
+                currentObject.includeNumber.is = false;
+                removeItem(currentObject.includeNumber.range)
+            } else if(conditionName == 'symbol') {
+                currentObject.includeSymbol.is = false;
+                removeItem(currentObject.includeSymbol.range)
             }
         }
+    
+        setCondition(currentObject);
     }
     let handleSlider = (e) => {
         
         setLength(e.target.value)
     }
+    let setConditionRange = (range) => {
+        setArrayOfSelection(prev => [
+            ...prev, 
+            range
+        ])
+    }
+    useEffect(()=> {
+        let subArray = null
+        if(condition.includeLower.is ) {
+            subArray = [97,122]
+            let exists = arrayOfSelection.some(arr => JSON.stringify(arr) === JSON.stringify(subArray))
+            if(exists == false) {
+                setConditionRange(subArray)
+            }
+        }
+        if(condition.includeUpper.is) {
+            subArray = [65,90]
+            let exists = arrayOfSelection.some(arr => JSON.stringify(arr) === JSON.stringify(subArray))
+            if(exists == false) {
+                setConditionRange(subArray)
+            }
+            
+        } else {
+        } 
+        if(condition.includeNumber.is) {
+            // arrayOfSelection.push([48,57])
+            subArray = [48,57]
+            let exists = arrayOfSelection.some(arr => JSON.stringify(arr) === JSON.stringify(subArray))
+            if(exists == false) {
+                setConditionRange(subArray) 
+            }
+            
+        } else {
+        } 
+        if(condition.includeSymbol.is) {
+            console.log("have symbol")
+            subArray = [[33,47],[58,64],[94,95], [124,126]]
+            let exists = arrayOfSelection.some(arr => JSON.stringify(arr) === JSON.stringify(subArray))
+            if(exists == false) {
+                setConditionRange(subArray)
+            }
+
+        } 
+    },[condition])
     return(
         <section id="Generator">
             <h1>Password generator</h1>
@@ -128,7 +175,7 @@ export default function Generator() {
                             <p>Include Uppercase Letters</p>
                         </li>
                         <li className="condition">
-                            <input onChange={e => handleChange(e)} name="lowercase" checked type="checkbox"/>
+                            <input onChange={e => handleChange(e)} name="lowercase"  type="checkbox"/>
                             <p>Include Lowercase Letter</p>
                         </li>
                         <li className="condition">
