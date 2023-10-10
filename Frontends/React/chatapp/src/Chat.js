4c915215da2767e090d99dc1f0c7df9d8baa9141
Pat leosrisook <49037327+PatLeosrisook@users.noreply.github.com>
2023-10-10 10:52:47 +0700

    changes in chat

--- Frontends/React/chatapp/src/Chat.js
+++ Frontends/React/chatapp/src/Chat.js
@@ -1,4 +1,12 @@
 import { ChatBubble } from "./ChatBubble"
+import {
+    MainContainer,
+    ChatContainer,
+    MessageList,
+    Message,
+    MessageInput,
+  } from "@chatscope/chat-ui-kit-react";
+  import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 export function Chat() {
     let handleKeyPress = (e) => {
         console.log(e)
@@ -8,15 +16,20 @@ export function Chat() {
     }
     return(
         <section id="Chat">
-            <div id="message_area">
-
-            </div>
-            <div className="card">
-                <div className="form_group">
-                    
-                    <textarea onKeyPress={handleKeyPress} name="Message"></textarea>
-                </div>
-            </div>
+            <MainContainer>
+                <ChatContainer>
+                <MessageList>
+                    <Message
+                    model={{
+                        message: "Hello my friend",
+                        sentTime: "just now",
+                        sender: "Joe",
+                    }}
+                    />
+                </MessageList>
+                <MessageInput placeholder="Type message here" />
+                </ChatContainer>
+            </MainContainer>
         </section>
     )
 }
