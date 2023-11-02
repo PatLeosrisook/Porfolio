7cecc0e3e2b8d80adf957fd0f1980d3b9a561220
Pat Leo <49037327+PatLeosrisook@users.noreply.github.com>
2023-11-02 21:07:26 +0700

    update any changes

--- Frontends/React/entertainment-web-app/entertainment-app/src/Pages/Movie.js
+++
@@ -1,9 +0,0 @@
-// This page is the template for each page
-export default function Movie({children, title}) {
-    return(
-        <section id={`${title}-section`}>
-            <h1>{title}</h1>
-            {children}
-        </section>
-    )
-}
