906a4744a9bf9db3229fcd4db5478b6379720d43
Pat Leo <49037327+PatLeosrisook@users.noreply.github.com>
2023-11-06 06:42:42 +0700

    styling

--- Frontends/React/personalsite/src/Components/Nav.js
+++ Frontends/React/personalsite/src/Components/Nav.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from 'react';
 import Logo from '../assets/Logo.svg'
+import UseAnimation from "react-useanimations";
+import menu2 from 'react-useanimations/lib/menu2'
 export function Nav() {
+    const [menuSize, setMenuSize] = useState(24)
+    const [isWindowResized, setIsWindowResized] = useState(false)
+    window.onresize = () => {
+        setIsWindowResized(true)
+    }
+    useEffect(() => {
+        let currentWidth = window.innerWidth
+        if(currentWidth < 700) {
+            setMenuSize(24)
+        } else if(currentWidth>= 700 && currentWidth < 1024) {
+            setMenuSize(40)
+        } else {
+            setMenuSize(50)
+        }
+        setIsWindowResized(false)
+    },[isWindowResized])
     return(
         <header>
             <img src={Logo} id="logo" />
-            <div className="icon-wrap">
-                <img src="" id="hamburger"/>
-            </div>
+            <UseAnimation animation={menu2} size={menuSize} strokeColor={"white"} />
             <nav id="Links">
                 <ul className="Links">
                     <li><a href="">Home</a></li>
--- Frontends/React/personalsite/src/Pages/Home.js
+++ Frontends/React/personalsite/src/Pages/Home.js
@@ -8,10 +8,35 @@ export function Home() {
                 <p id="Bg-text">Dev<br/>elo<br/>per</p>
             </section>
             <section id="About">
-
+                <div className="paragraphs">
+                    <p>I'm Pat, a 25-year-old computer science graduate from the University of Birmingham, UK. Currently, I'm working in IT at BSG Group. I'm passionate about technology, creativity, and continuous learning.</p>
+                    <p><span className="highlight">Professional summary</span> I'm dedicated to using technology to solve real-world problems and passionate about fostering creativity through art and literature.</p>
+                    <p><span className="highlight">Experience</span> In my current role at BSG Group, I ensure the smooth operation of our IT infrastructure. I have a deep understanding of IT management.</p>
+                    <p><span className="highlight">Skills</span> Proficient in software development, web design, and data analysis. Expertise in Java, Python, HTML, CSS, and JavaScript.</p>
+                    <p><span className="highlight">Interests</span> I'm an artist who enjoys drawing, an avid reader, and a web development enthusiast. My aim is to bridge technology and creativity.</p>
+                </div>
+                <p className="BG-text" id="About-bg">About</p>
             </section>
             <section id="Contact">
-
+                
+                <p className="BG-text">
+                    Contact
+                </p>
+                <form>
+                    <div className="formGroup">
+                        <label for="name">Name</label>
+                        <input name="name" id="name" />
+                    </div>
+                    <div className="formGroup">
+                        <label for="email">Email</label>
+                        <input name="email" id="email" />
+                    </div>
+                    <div className="formGroup">
+                        <label for="message">Message</label>
+                        <textarea name="message"></textarea>
+                    </div>
+                    <button id="Submit" type="submit">Send</button>
+                </form>
             </section>
         </section>
     )
