ef1babd9d56e9d5d185d30a8d84ed0c325cbf49a
Pat Leo <49037327+PatLeosrisook@users.noreply.github.com>
2024-03-08 06:42:15 +0700

    building mock authentication

--- Frontends/NextJS/streaming/src/app/(auth)/layout.tsx
+++ Frontends/NextJS/streaming/src/app/(auth)/layout.tsx
@@ -1,8 +1,21 @@
+'use client';
+import Image from "next/image"
+import Link from "next/link"
+import { useState } from "react";
 export default function RegisterLayout({children} : {
     children: React.ReactNode
 }) {
+    const[value, setValue] = useState({
+        Email:"",
+        Password:"",
+    })
     return (
         <section id="Registrations">
+            <header id="Register_header">
+                <Link href="/">
+                    <Image src={'/icons/Movie.svg'} width={20} height={20} alt="Logo" />
+                </Link>
+            </header>
             <form className="Register_card">
                 {children}
             </form>
--- Frontends/NextJS/streaming/src/app/(auth)/login/page.tsx
+++ Frontends/NextJS/streaming/src/app/(auth)/login/page.tsx
@@ -1,11 +1,16 @@
+'use client';
 import Link from "next/link";
 
 export default function Login() {
+    let handleChange = (e) => {
+        console.log(e.target.value)
+       
+    }
     return (
         <>
             <h2 className="card_title">Login</h2>
             <div className="form_group">
-                <input name="Email" placeholder="Email address"/>
+                <input onChange={e => handleChange(e)} name="Email" placeholder="Email address"/>
             </div>
             <div className="form_group">
                 <input name="password" type="password" placeholder="Password"/>
