53a2ad9738c5a8fe1c8cc076b317b06d05da72a3
Pat Leo <49037327+PatLeosrisook@users.noreply.github.com>
2024-05-15 15:45:26 +0700

    start using tailwind

--- Frontends/React/bmi/src/App.js
+++ Frontends/React/bmi/src/App.js
@@ -3,22 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <section id="Head" className='w-full flex flex-row'>
+        <article className='Headline w-1/2 flex flex-column item-start'  >
+          <h1>Body Mass Index Calculator</h1>
+          <p>Better understand your weight in relation to your height using
+            our body mass index (BM) calculator. While BMI is not the
+            sole determinant of a healthy weight, it offers a valuable
+            starting point to evaluate your overall health and well-being.</p>
+        </article>
+        <article id="Calculator" className='w-1/2'>
+            <form>
+              <div className='form_group unit'>
+
+              </div>
+              <div className='form_group '>
+
+              </div>
+            </form>
+            <div id="Banner">
+              <p>Welcome!</p>
+              <p>Enter your height and weight and you'll see your BMI result here</p>
+            </div>
+        </article>
+      </section>
+      <section id="BMI">
+
+      </section>
+      <section id="limitation">
+
+      </section>
+    </section>
   );
 }
 
