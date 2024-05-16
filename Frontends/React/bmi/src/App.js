871f8283a83415ecdbd0a3379c23f3fba17689fd
Patleo <kalyadaleosrisook@gmail.com>
2024-05-16 17:53:55 +0700

    styling

--- Frontends/React/bmi/src/App.js
+++ Frontends/React/bmi/src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBowlFood, faDumbbell, faMoon } from  '@fortawesome/free-solid-svg-icons'
+import foodPortrait from './assets/FoodByMonika.jpg'
 function App() {
   return (
     <section className="App relative">
-    <div className='bg-teal-400 w-[683px] h-[683px] rounded-full absolute -left-[162px] -top-[180px] z-0'>
+    <div className='bg-gradient-to-br from-slate-50 to-blue-300 w-[683px] h-[683px] rounded-full absolute -left-[162px] -top-[180px] z-0'>
 
     </div>
     <header className='w-full h-[15vh]'>
 
     </header>
-      <section id="Head" className='w-full flex flex-col h-fit  items-center justify-center gap-20'>
+      <section id="Head" className='w-full flex flex-col h-fit  items-center justify-center gap-20 bg-gradient-to-b from-slate-50 to-sky-100'>
         <article className='Headline w-full flex flex-col items-center relative z-10'  >
           <h1 className='text-6xl'>Body Mass Index Calculator</h1>
           <p className='text-base'>Better understand your weight in relation to your height using
@@ -52,11 +54,48 @@ function App() {
             </div>
         </article>
       </section>
-      <section id="BMI" className='h-screen w-full'>
-
+      <section id="BMI" className='h-fit w-full min-h-screen bg-gradient-to-b from-slate-50 to-sky-100'>
+      <div className="flex flex-col items-center justify-center">
+          <img src={foodPortrait} alt="Image of a food"/>
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="text-2xl font-semi-bold">What's your BMI result means</h3>
+            <p className="text-left">A BMI range of 18.5 to 24.9 is considered a 'healthy weight'. Maintaining a healthy weight may lower your chance of experiencing health issue later on, such as obesity and type 2 diabetes. Aim for a nutritiuos diet with reduced fat and sugar content, incorporate ample fruites and vegetables. Additionally, strive for physical activities, ideally for 30 minutes daily for five days a week. </p>          
+          </div>
+          
+        </div>
+        {/* list */}
+      <div className="flex flex-col w-[90vw] h-screen">
+            {/* Lists of how to loose weight */}
+            <div className="flex flex-col items-start justify-center w-100 h-fit gap-[15px]">
+              <div className="flex flex-col items-center justify-center w-[50px] h-[50px] rounded-full bg-pink-200">
+                {/* Icon */}
+                <FontAwesomeIcon icon={faBowlFood} className='text-pink-600'/>
+              </div>
+              <h4 className='text-2xl font-bold'>Healthy eating</h4>
+              <p className='text-left'>Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity , and mood.</p>
+            </div>
+            {/* regular ex */}
+            <div className="flex flex-col items-start justify-center w-100 h-fit gap-[15px]">
+              <div className="flex flex-col items-center justify-center w-[50px] h-[50px] rounded-full bg-yellow-100">
+                {/* Icon */}
+                <FontAwesomeIcon icon={faDumbbell}  className='text-yellow-600'/>
+              </div>
+              <h4 className='text-2xl font-bold'>Regular exercise</h4>
+              <p className='text-left'>Exercise imporve fitness, aids weight control, elevate mood, and reduce disease risk, fostering wellness and longetiviy.</p>
+            </div>
+            {/* Adequte sleep */}
+            <div className="flex flex-col items-start justify-center w-100 h-fit gap-[15px]">
+              <div className="flex flex-col items-center justify-center w-[50px] h-[50px] rounded-full bg-teal-200">
+                {/* Icon */}
+                <FontAwesomeIcon icon={faMoon}  className='text-teal-600'/>
+              </div>
+              <h4 className='text-2xl font-bold'>Adequate sleep</h4>
+              <p className='text-left'>Sleep enhance mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.</p>
+            </div>
+      </div>
       </section>
-      <section id="limitation h-screen w-full">
-
+      <section id="limitation h-screen w-full  bg-gradient-to-b from-slate-50 to-sky-100">
+        
       </section>
     </section>
   );
