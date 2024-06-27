78349d9ba23f43a623017950f24a2b86c34b6de4
Pat Leo <49037327+PatLeosrisook@users.noreply.github.com>
2024-06-27 11:43:17 +0700

    accoutn setting

--- Frontends/NextJS/streaming/src/app/[user]/Account/account_setting/page.tsx
+++ Frontends/NextJS/streaming/src/app/[user]/Account/account_setting/page.tsx
@@ -1,8 +1,30 @@
-
+'use client';
+import { getUser } from '@/lib/getUser'
+import { useEffect, useState } from 'react'
 export default function account_setting() {
+    const currentEmail = async() => {
+        return await getUser()
+    }
+    const [email, setEmail] = useState("")
+    useEffect(() => {
+
+        currentEmail().then((email) => {
+            setEmail(email.currentEmail)
+        })
+    })
     return ( 
         <section id="Account_setting">
             <p>Account setting</p>
+
+            <div className="form_group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name='email' placeholder={email} />
+            </div>
+            <div className="form_group update_password" >
+                <input type="password" placeholder={"Old password"} />
+                <input type="password" placeholder={"New password"} />
+                <input type="password" placeholder={"Confirm new password"} />
+            </div>
         </section>
     )
 }
--- Frontends/NextJS/streaming/src/lib/getUser.tsx
+++ Frontends/NextJS/streaming/src/lib/getUser.tsx
@@ -3,6 +3,7 @@ export async function getUser() {
     let user = await axios.get('/api/users/me')
     
     return {
-            currentUser: user.data.data.username
+            currentUser: user.data.data.username,
+            currentEmail:user.data.data.email
     }
 }
