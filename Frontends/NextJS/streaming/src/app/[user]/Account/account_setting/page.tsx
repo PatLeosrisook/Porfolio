12957db421dedb96063a45296cec9bc9f357cce7
Pat Leo <49037327+PatLeosrisook@users.noreply.github.com>
2024-06-28 11:09:05 +0700

    account page

--- Frontends/NextJS/streaming/src/app/[user]/Account/account_setting/page.tsx
+++ Frontends/NextJS/streaming/src/app/[user]/Account/account_setting/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { getUser } from '@/lib/getUser'
+import axios from 'axios';
 import { useEffect, useState } from 'react'
 export default function account_setting() {
     const currentEmail = async() => {
@@ -12,6 +13,10 @@ export default function account_setting() {
             setEmail(email.currentEmail)
         })
     })
+
+    const handleSaveChange = async() => {
+        await axios('/api/users/update_profile')
+    }
     return ( 
         <section id="Account_setting">
             <p>Account setting</p>
@@ -21,10 +26,12 @@ export default function account_setting() {
                 <input type="email" name='email' placeholder={email} />
             </div>
             <div className="form_group update_password" >
+                <label htmlFor="password">Password</label>
                 <input type="password" placeholder={"Old password"} />
                 <input type="password" placeholder={"New password"} />
                 <input type="password" placeholder={"Confirm new password"} />
             </div>
+            <button className='save_change'>save change</button>
         </section>
     )
 }
