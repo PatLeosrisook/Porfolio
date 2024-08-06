a245bfa42714f2e08e4ec447dbb362fc1fea1036
Patleo <kalyadaleosrisook@gmail.com>
2024-08-06 17:31:48 +0700

    passing data to another page

--- Frontends/NextJS/streaming/src/Component/GenreOverview.tsx
+++ Frontends/NextJS/streaming/src/Component/GenreOverview.tsx
@@ -1,15 +1,20 @@
 import RecommendedMedia from "./RecommendedMedia"
 import Link from "next/link"
+import { useRouter } from "next/router"
 export default function GenreOverview({Genre, lists, link} : {Genre: string, lists: any, link: string}) {
+    const router = useRouter()
     let handleNavigate = (e) => {
-
+        router.push({
+            pathname: link,
+            query: { genre: JSON.stringify(lists) }
+        })
     }
     return (
         <section className="genre">
             <section className="genre-header">
                 <h3>{Genre}</h3>
-                <div className="link-genre">
-                    <Link href={link}>view more</Link>
+                <div onClick={handleNavigate} className="link-genre">
+                    View more
                 </div>
             </section>
             <section className="genre-list">
--- Frontends/NextJS/streaming/src/app/[user]/Movie/[genre]/page.tsx
+++ Frontends/NextJS/streaming/src/app/[user]/Movie/[genre]/page.tsx
@@ -1,6 +1,13 @@
 
-
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 export default function Genre() {
+    const router = useRouter() 
+    const {genre} = router.query
+
+    useEffect(() => {
+        
+    })
     return(
         <section>
             <h1>Sub movie</h1>
